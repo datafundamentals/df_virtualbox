47643f335ed458443849e1fad7a1380a699862e8
jcarapet <jeffcarapetyan@gmail.com>
2014-02-18 16:12:55 -0600

    installs virtualbox but does not install it fully working with headless displays yet

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,14 @@
 # 
 # All rights reserved - Do Not Redistribute
 #
+
+
+
+# the first step is making sure that everything needed is installed on the system
+package "virtualbox" do 
+	source node['virtualbox']['source']
+	action :install 
+end
+
+# there will likely be some configuration of kernel modules
+# we will get to that later however
